eba9559895292b5ac1924b649f685e73710f8cea
DmitryDutov <dmitry_dutov@inbox.ru>
2023-10-04 07:47:44 +0300

    Создали DbContext, зарегистрировали сервис

---
+++ WebApiServiceForBlazor/DataBase/WeatherDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiServiceForBlazor.Models;
+
+namespace WebApiServiceForBlazor.DataBase
+{
+    public class WeatherDbContext : DbContext
+    {
+        public DbSet<WeatherForecast> Forecasts { get; set; }
+
+        public WeatherDbContext(DbContextOptions<WeatherDbContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
+
--- WebApiServiceForBlazor/Program.cs
+++ WebApiServiceForBlazor/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiServiceForBlazor.DataBase;
 using WebApiServiceForBlazor.Services;
 using WebApiServiceForBlazor.Services.Interfaces;
 
@@ -9,6 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DB
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<WeatherDbContext>(options => options.UseSqlServer(connection));
+
+//Service
 builder.Services.AddScoped<IWeatherForecastService, WeatherForecastRussianService>();    // Ёкземпл€р сервиса создаЄтс€ при обращении к сервису и живЄт до окончани€ выполнени€ задчи
 //builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>(); // Ёкземпл€р сервиса создаЄтс€ при каждом обращении к сервису даже если это происходит одновременно. Ёкземпл€р живЄт до окончани€ выполнени€ задачи
 //builder.Services.AddSingleton<IWeatherForecastService, WeatherForecastService>(); // Ёкземпл€р сервиса создаЄтс€ при запуске приложени€ и живЄт до окончани€ работы приложени€
